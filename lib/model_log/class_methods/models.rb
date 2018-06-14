70b4886360655fe98866c10b6babad2dcafa3b07
OrderSun <462436208@qq.com>
2018-06-14 10:00:11 +0800

    调整日志格式；优化部分代码

--- lib/model_log/class_methods/models.rb
+++ lib/model_log/class_methods/models.rb
@@ -28,7 +28,7 @@ module ModelLog
         end
 
         def data_processing(field_name, field_data, **options)
-          if defined? CarrierWave::Uploader::Base and field_data.is_a? CarrierWave::Uploader::Base
+          if defined? CarrierWave::Uploader::Base  and field_data.is_a? CarrierWave::Uploader::Base
             return options[:index] == 0 ? get_uploader_filename(field_data.url) : send("#{field_name}_identifier")
           elsif field_data.is_a? BigDecimal
             return field_data.to_f
@@ -46,52 +46,50 @@ module ModelLog
         end
 
         def write_log(resource, type)
-          begin
-            passport = ModelLog::Passport.current_passport
-            request = ModelLog::Request.current_request
-            type = type.to_s
-            can_write = true
-            content_text = {}
+          passport = ModelLog::Passport.current_passport
+          request = ModelLog::Request.current_request
+          type = type.to_s
+          can_write = true
+          content_text = {}
 
-            if type == "update"
-              changes = resource.changes
-              changes.each do |key, values|
-                value_arr = []
-                values.each_with_index do |value, i|
-                  value_arr << data_processing(key, value, :index => i)
-                end
-                content_text.merge!(Hash[key, value_arr])
+          if type == "update"
+            changes = resource.changes
+            changes.each do |key, values|
+              value_arr = []
+              values.each_with_index do |value, i|
+                value_arr << data_processing(key, value, :index => i)
               end
-
-              can_write = false if content_text.empty?
-            else
-              resource.class.content_columns.each do |column|
-                column_name = column.name
-                column_val = data_processing(column_name, resource.send(column_name))
-                content_text.merge!(Hash[column_name, column_val])
-              end
+              content_text.merge!(Hash[key, value_arr])
             end
 
-            file_content = <<-EFO.strip_heredoc.gsub("\n", "\t")
-              #{request.try(:request_method)}
-              #{request.try(:url)}
-              #{request.try(:referer)}
-              #{request.try(:remote_ip)}
-              #{request.try(:user_agent)}
-              #{ModelLog.platform}
-              #{passport.try(:id)}
-              #{passport.try(ModelLog.user_name_field)}
-              #{type.to_s}
-              #{resource.class}
-              #{resource.id}
-              #{Time.now}
-              #{content_text}
-            EFO
-
-            ModelLog.info file_content if can_write
-          rescue => e
-            ModelLog.info e.message
+            can_write = false if content_text.empty?
+          else
+            resource.class.content_columns.each do |column|
+              column_name = column.name
+              column_val = data_processing(column_name, resource.send(column_name))
+              content_text.merge!(Hash[column_name, column_val])
+            end
           end
+
+          file_content = <<-EFO.strip_heredoc.gsub("\n", "\t")
+            #{Time.now.strftime("%Y-%m-%d %H:%M:%S")}
+            #{request.try(:request_method)}
+            #{request.try(:url)}
+            #{request.try(:referer)}
+            #{request.try(:remote_ip)}
+            #{request.try(:user_agent)}
+            #{ModelLog.platform}
+            #{passport.try(:id)}
+            #{passport.try(ModelLog.user_name_field)}
+            #{type}
+            #{resource.class}
+            #{resource.id}
+            #{content_text}
+          EFO
+
+          ModelLog.info file_content if can_write
+        rescue => e
+          ModelLog.info e.message
         end
     end
   end
