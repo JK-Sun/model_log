ae8f04b2a7501075b0c92d957ce81eb882e354e4
JK-Sun <jk-sun@qq.com>
2019-12-20 23:17:34 +0800

    feat: change model_log

--- lib/model_log.rb
+++ lib/model_log.rb
@@ -1,22 +1,38 @@
 require "model_log/version"
-require "model_log/passport"
-require "model_log/request"
-require "model_log/class_methods/models"
-require "model_log/class_methods/controllers"
+require "model_log/store"
+require "model_log/controller"
+require "model_log/modeller"
+require "model_log/logger"
+require "model_log/log/base"
+require "model_log/log/content"
+require "model_log/log/processor"
 
 module ModelLog
   class << self
-    attr_accessor :platform, :current_user_method, :user_name_field
+    attr_accessor :current_user_fn, :identity_field
 
-    def info(content)
-      content = content.chomp + "\n"
-      File.open("log/model_log_#{Rails.env}.log", "a+") do |output|
-        output.write(content)
-      end
+    def logger
+      @logger ||= Logger.new("model_log_#{Rails.env}.log")
+    end
+
+    def requester
+      Store.current_requester
+    end
+
+    def requester_exist?
+      !!requester
+    end
+
+    def current_user
+      Store.current_user
+    end
+
+    def current_user_exist?
+      !!current_user
     end
   end
 
-  @current_user_method = @current_user_method || :current_passport
-  @user_name_field = @user_name_field || :username
+  @current_user_fn ||= :current_user
+  @identity_field ||= :id
 end
 
