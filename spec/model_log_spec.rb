bae80900fb380ec57f48e272f7b106d6448f7c3f
JK-Sun <jk-sun@qq.com>
2019-12-22 18:47:26 +0800

    del: delete spec

--- spec/model_log_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe ModelLog do
-  it "has a version number" do
-    expect(ModelLog::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
