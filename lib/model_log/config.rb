8b64ed2307fec37b2e8d6098ae7f8b9cd828a4b5
JK-Sun <jk-sun@qq.com>
2019-12-28 13:10:34 +0800

    fix: modify logger implementation

--- lib/model_log/config.rb
+++ lib/model_log/config.rb
@@ -1,12 +1,14 @@
 module ModelLog
   class Config
-    attr_accessor :current_user_method, :identity_field, :separator, :formatter
+    attr_accessor :current_user_method, :identity_field, :separator, :formatter,
+                  :logger_level, :logger_datetime_format
 
     def initialize
       @current_user_method ||= :current_user
       @identity_field      ||= :id
       @separator           ||= "\t"
       @formatter           ||= Default::Formatter
+      @logger_level        ||= :info  # :debug < :info < :warn < :error < :fatal < :unknown
     end
   end
 end
